962fc63b46596a821fd46603792d05f064884155
Ross O'Reilly <oreillyross@gmail.com>
2021-12-29 17:09:32 +0100

    up to speed with Will course

--- hipster/frontend/components/Header.js
+++ hipster/frontend/components/Header.js
@@ -1,16 +1,50 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  background-color: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  z-index: 2;
+  transform: skew(-7deg);
+  position: relative;
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyle = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyle>
       <div className="bar">
-        <Link href="/">Hipster</Link>
+        <Logo>
+          <Link href="/">Hipster </Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>search bar goes here</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyle>
   );
 }
